e9814d6bbeb927c17e308a770d10b58fbcea02ef
Zorig Magnaituvshin <zorigmagnaituvshin@gmail.com>
2017-07-07 14:56:52 -0400

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,17 @@
+package endstarttesting;
+
+import java.util.Arrays;
+
+public class Anagram {
+
+	public boolean isAnagram(String Str, String Str2)
+	{
+		char[] chars1 = Str.toCharArray();
+		char[] chars2 = Str2.toCharArray();
+		Arrays.sort(chars1);
+		Arrays.sort(chars2);
+		//System.out.println(chars1[1] + " " + chars2[1]);
+		
+		return new String(chars1).equals(new String(chars2));
+	}
+}
